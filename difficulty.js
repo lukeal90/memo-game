2632c57e1d14edb53e6fbfdc18b2811229c2aa41
lukeal90 <lainolucas90@gmail.com>
2022-06-02 13:16:47 -0300

    Logica nivel pasado

--- difficulty.js
+++ difficulty.js
@@ -1,7 +1,90 @@
-const difficulty = {
-    "easy" : 5,
-    "normal" : 7,
-    "hard" : 10
-}
+export const levels = [
+    {
+        difficulty: 'easy',
+        props: [{
+                nivel: 1,
+                speed: 500,
+                squareCant: 3
+            },
+            {
+                nivel: 2,
+                speed: 400,
+                squareCant: 3
+            },
+            {
+                nivel: 3,
+                speed: 400,
+                squareCant: 4
+            },
+            {
+                nivel: 4,
+                speed: 300,
+                squareCant: 4
+            },
+            {
+                nivel: 5,
+                speed: 300,
+                squareCant: 5
+            },
+        ]
+    },
+    {
+        difficulty: 'normal',
+        props: [{
+                nivel: 1,
+                speed: 500,
+                squareCant: 5
+            },
+            {
+                nivel: 2,
+                speed: 400,
+                squareCant: 5
+            },
+            {
+                nivel: 3,
+                speed: 400,
+                squareCant: 6
+            },
+            {
+                nivel: 4,
+                speed: 300,
+                squareCant: 6
+            },
+            {
+                nivel: 5,
+                speed: 300,
+                squareCant: 7
+            },
+        ]
+    },
+    {
+        difficulty: 'hard',
+        props: [{
+                nivel: 1,
+                speed: 500,
+                squareCant: 7
+            },
+            {
+                nivel: 2,
+                speed: 400,
+                squareCant: 7
+            },
+            {
+                nivel: 3,
+                speed: 400,
+                squareCant: 8
+            },
+            {
+                nivel: 4,
+                speed: 300,
+                squareCant: 8
+            },
+            {
+                nivel: 5,
+                speed: 300,
+                squareCant: 9
+            },
+        ]
+    },    
+];
 
-export default difficulty;
--- index.js
+++ index.js
@@ -1,82 +1,229 @@
+// import {levels} from './difficulty.js'
+// import {startGame} from './game.js'
+
+// console.log(levels)
+const difficultys = [
+    {
+        difficulty: 'easy',
+        props: [{
+                nivel: 1,
+                speed: 500,
+                squareCant: 3,
+                score: 100
+            },
+            {
+                nivel: 2,
+                speed: 400,
+                squareCant: 3,
+                score: 150
+            },
+            {
+                nivel: 3,
+                speed: 400,
+                squareCant: 4,
+                score: 225
+            },
+            {
+                nivel: 4,
+                speed: 300,
+                squareCant: 4,
+                score: 275
+            },
+            {
+                nivel: 5,
+                speed: 300,
+                squareCant: 5,
+                score: 300
+            },
+        ]
+    },
+    {
+        difficulty: 'normal',
+        props: [{
+                nivel: 1,
+                speed: 500,
+                squareCant: 5,
+                score: 300
+            },
+            {
+                nivel: 2,
+                speed: 400,
+                squareCant: 5,
+                score: 350
+            },
+            {
+                nivel: 3,
+                speed: 400,
+                squareCant: 6,
+                score: 400
+            },
+            {
+                nivel: 4,
+                speed: 300,
+                squareCant: 6,
+                score: 450
+            },
+            {
+                nivel: 5,
+                speed: 300,
+                squareCant: 7,
+                score: 500
+            },
+        ]
+    },
+    {
+        difficulty: 'hard',
+        props: [{
+                nivel: 1,
+                speed: 500,
+                squareCant: 7,
+                score: 500
+            },
+            {
+                nivel: 2,
+                speed: 400,
+                squareCant: 7,
+                score: 600
+            },
+            {
+                nivel: 3,
+                speed: 400,
+                squareCant: 8,
+                score: 700
+            },
+            {
+                nivel: 4,
+                speed: 300,
+                squareCant: 8,
+                score: 800
+            },
+            {
+                nivel: 5,
+                speed: 300,
+                squareCant: 9,
+                score: 1000
+            },
+        ]
+    },    
+]
+
 // Seleccion de dificultad
 let selectDifficulty = document.getElementById('difficulty');
-let difficulty = 5;
+let difficulty = 0;
 
 selectDifficulty.addEventListener('change', () => {
     difficulty = parseInt(selectDifficulty.value);
 });
 
-// Funcion start game
+window.localStorage.clear();
+
+//Funcion start game
 const startGame = async () => {
-    resetGameColor();
-    await startCounter();
+    const player = "Lucas"
+    savePlayerData(player,0,0);
+    const maxLevel = difficultys[difficulty].props.length;
     let positions = [];
-
-    positions = squareGenerator(difficulty);
+    let difficultySelected = difficultys[difficulty];
+    let level = 1;
+    let velocity;
     const grilla = document.getElementById('grilla');
-
-    console.log(positions);
-    // To do : Aca agregar un conteo 3,2,1 Start!
-    // Arranca el juego prende las luces
-    lightsGameOn(positions, 500);
+    let playerLoose = false;
     // To do :hasta que no termine la secuencia que no continue este paso
     // y no pueda clickear nada
+    await lightsGameOn(positions, velocity);
 
-    // Cambiar por onClick
-    grilla.onclick = (c) => {
-        let positionToCompare = positions.shift();
-        console.log("Es esta posicion " , positionToCompare)
-        let positionClicked = c.target;
-        console.log("Aprete esta " ,c.target.id)
-        console.log("Quedan " ,positions)
-        if(positionClicked.id == positionToCompare){
-            changeColor(positionClicked)
-            positions.length == 0 ? console.log('Ganaste!') : false;
-        }else{
-            changeColor(positionClicked, true)
-            console.log("Perdiste! Volve a intentar!")
-        }        
-    };
+    while(level <= maxLevel && !playerLoose) {
+        console.log(`Comienzas el nivel: ${level}` );
+        velocity = difficultySelected.props[level-1].speed;
+        positions = squareGenerator(difficultySelected.props[level-1].squareCant); 
+        console.log(positions)
+        await resetGameColor(500);
+        await startCounter();   
+        await lightsGameOn(positions, velocity);
+        while(positions.length > 0 && !playerLoose) {
+            let correctPosition = await waitForClicks(positions, grilla);
+            !correctPosition ? playerLoose = true : false;
+        }
+        let score = difficultySelected.props[level-1].score;
+        savePlayerData(player,score,1);
+        level+= 1;
+        
+    }
+    console.log("Termino el juego")  
+  //  await resetGameColor(2000);
+    console.log("termino de resetear color")
 }
 
-// No funca ERROR RARO ver mañana. 
+const createPlayer = (name,score,level) => {
+    return {
+        name: name,
+        score: score,
+        levelsPassed: level
+    }
+}
 
-// const checkSquareClicked = ( position, clicked ) => {
-//     if(position == clicked){
-//         changeColor(position);
-//     }else{
-//         changeColor(position, true);
-//         console.log("Perdiste! Volve a intentar!");
-//     }
-// }
+const savePlayerData = (playerName, score, level) => {
+    let playerSaved = localStorage.getItem('player');
+
+    if(playerSaved == null){
+        playerSaved = createPlayer(playerName, score, level);
+    }else{
+        playerSaved = JSON.parse(playerSaved);
+        playerSaved['score'] = playerSaved['score'] + score;
+        playerSaved['levelsPassed'] = playerSaved['levelsPassed'] + level;
+    }
+    window.localStorage.setItem('player', JSON.stringify(playerSaved));
+}
+
+const waitForClicks = (positions, grilla) => {
+    return new Promise((resolve) =>{
+        grilla.onclick = (c) => {
+            let positionToCompare = positions.shift();
+            let positionClicked = c.target;
+            let correctPosition = checkSquareClicked(positionClicked, positionToCompare, positions);
+            resolve(correctPosition);
+        }
+    });
+}
+
+const checkSquareClicked = (positionClicked, positionToCompare, positions) => {
+    let correctPosition = true;
+    if (positionClicked.id == positionToCompare) {
+        changeColor(positionClicked)
+    } else {
+        changeColor(positionClicked, true)
+        correctPosition = false;
+        console.log("Perdiste! Fin del juego!")
+    }
+    return correctPosition;
+}
 
 // Prende la secuencia de luces del juego y recibe la dificultad de velocidad
 const lightsGameOn = async (positions, difficultyTime) => {
-    for(const position of positions){
-        await lightsGame(position, difficultyTime);        
-    }
+        for (const position of positions) {
+            await lightsGame(position, difficultyTime);
+        }
 }
 
 // Logica de cambio de color de los cuadrados
 const lightsGame = (index, time) => {
-        return new Promise( (resolve)=>{
-            let square = document.getElementById(index);
-            setTimeout(changeColor.bind(null, square), 1000);
-            setTimeout(changeColor.bind(null, square), 1100);
-            setTimeout(() => {
-                resolve();
-            }, time);     
-        }
-    ) 
+    return new Promise((resolve) => {
+        let square = document.getElementById(index);
+        setTimeout(changeColor.bind(null, square), 400);
+        setTimeout(changeColor.bind(null, square), 500);
+        setTimeout(() => {
+            resolve();
+        }, time);
+    })
 }
 
 // Cambia el color del cuadradrado
 const changeColor = (square, error = false) => {
-    if(!error){
+    if (!error) {
         let audio = document.getElementById("audio");
         square.className = square.className == 'cuadrado' ? 'cuadradoOn' : 'cuadrado';
         square.className == 'cuadradoOn' ? audio.play() : false;
-    }else{
+    } else {
         square.className = 'cuadradoError';
         let audio = document.getElementById("audioError");
         audio.play()
@@ -84,66 +231,73 @@ const changeColor = (square, error = false) => {
 }
 
 // Resetea todas las luces cuando se pone Start
-const resetGameColor = () => {
-    for (let i = 1; i < 16; i++) {
-        let square = document.getElementById(i);
-        square.className = 'cuadrado'
-    }
+const resetGameColor = (time) => {
+    return new Promise((resolve) => {
+        for (let i = 1; i < 16; i++) {
+            let square = document.getElementById(i);
+            square.className = 'cuadrado'
+        }
+        setTimeout(() => {
+            resolve();
+        }, time);
+    })
 }
 
 // Secuencia animada del juego
-const lightsSequence = async (time) =>{
+const lightsSequence = async (time) => {
     let i = 1;
 
-    if(time == 0){
+    if (time == 0) {
         return
     }
-    while(i <= 16){
+    while (i <= 16) {
         let index = i.toString();
         await lightsGame(i, time);
         i++
-    i > 16 ? lightsSequence(time) : false ;    
+        i > 16 ? lightsSequence(time) : false;
     }
 }
 
 // Generador de valores para el juego
 const squareGenerator = (cantSquares) => {
-    return Array.from({length: cantSquares}, () => Math.floor(Math.random() * 16 + 1));
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    for (let i = numbers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+    }
+    return numbers.slice(0, cantSquares);
 }
-      
+
 //lightsSequence(80);
 
 // To Do:
 
-// Si se repite la misma luz volver a repetir sonido.
-
 // Falta que cuando se gane se prenda todas las luces de verde y msj ganador.
-
-// Intentos por si falla. 3 vidas.
 
 // Error raro cuando se vuelve a mandar start no refreshea bien el array
 // y consulta por el anterior y el actual CORREGIR.
 
 // Que no se puede apretar cuadrados hasta que termine la secuencia.
 
-// Si pasa de nivel aumentar la velocidad ( 5 levels por dificultad )
-
 const secondsCounter = async (counterModal) => {
     let second = 3;
     return await new Promise(resolve => setInterval(() => {
-        if(second > 0){
+        if (second > 0) {
             counterModal.textContent = second;
             second--
-        }else{
+        } else if (second == 0) {
+            counterModal.textContent = "Go!"
+            second--;
+        } else if (second < 0) {
             clearInterval()
             resolve();
-        }        
-    },1000));
+        }
+    }, 1000));
 }
 
 const startCounter = async () => {
     let counterModal = document.createElement('div');
-    counterModal.className = 'counter';
+    counterModal.setAttribute('id', 'counter');
     let modalInside = document.createElement('div');
     modalInside.className = 'counter-inside';
     let seconds = document.createTextNode("");
@@ -153,9 +307,9 @@ const startCounter = async () => {
     document.body.appendChild(counterModal)
     await secondsCounter(modalInside);
 
-    counterModal = document.getElementsByClassName('counter')
-    console.log(counterModal)
-    document.body.removeChild(counterModal)
+    counterModal = document.getElementById('counter')
+    counterModal.remove();
+    //document.body.removeChild(counterModal)
 }
 // Agregar limite de tiempo para apretar cuadrado, sino pierde. 
 
@@ -164,4 +318,3 @@ const startCounter = async () => {
 // Guardar nombre de la persona y puntaje.
 
 // Si pasa de nivel aumentar la velocidad ( 5 levels por dificultad )
-
